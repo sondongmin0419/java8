b866c6822af281e8872c098c17967fdd460f0ab9
동민 <@DESKTOP-1HIAO4E>
2020-03-17 14:03:14 +0900

    4일차 : system.in.read() 예제

---
+++ java8/src/java8/day0402.java
@@ -0,0 +1,14 @@
+package java8;
+
+import java.io.IOException;
+
+public class day0402 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("키보드를 눌러서 키를 입력해 주세요.");
+		int a = System.in.read();
+		System.out.println(a);
+		
+	}
+
+}
